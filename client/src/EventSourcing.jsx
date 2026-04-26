9efe3b41dcd6635cbbd7fb32c2c8b68b919d9030
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-26 22:27:59 +0300

    feat: create UI for event sourcing

---
+++ client/src/EventSourcing.jsx
@@ -0,0 +1,48 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const EventSourcing = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+
+    async function subscribe() {
+        const eventSource = new EventSource('http://localhost:5000/connect');
+        eventSource.onmessage = (event) => {
+            console.log(JSON.stringify(event.data));
+        }
+    }
+
+    useEffect(() => {
+        void subscribe();
+    }, []);
+
+    const sendMessage = async () => {
+        await axios.post("http://localhost:5000/new-message", {
+            message: value,
+            id: Date.now(),
+        })
+    }
+
+    return (
+        <div className="center">
+            <div>
+                <div className="form">
+                    <input value={value}
+                           type="text"
+                           onChange={e => setValue(e.target.value)}
+                    />
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map((mess) => (
+                        <div key={mess.id} className="message">
+                            {mess.message}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EventSourcing;
