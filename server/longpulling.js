86dd25641264791f89459ac3a6a70415442200c4
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-26 22:05:14 +0300

    fix: fix routes for endpoints

--- server/longpulling.js
+++ server/longpulling.js
@@ -10,13 +10,13 @@ const app = express();
 
 app.use(cors());
 
-app.get('get-message', (req, res) => {
+app.get('/get-message', (req, res) => {
     emitter.once('newMessage', (message) => {
         res.json(message);
     });
 });
 
-app.post('new-message', (req, res) => {
+app.post('/new-message', (req, res) => {
     const message = req.body;
 
     emitter.emit('newMessage', message);
