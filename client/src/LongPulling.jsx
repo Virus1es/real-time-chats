76ed725ace95a72897dd253663942c61755db837
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-26 22:12:11 +0300

    feat: output message to UI

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -1,9 +1,26 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 const LongPulling = () => {
     const [messages, setMessages] = useState([]);
     const [value, setValue] = useState('');
+
+    useEffect(() => {
+        void subscribe();
+    }, [])
+
+    async function subscribe() {
+        try{
+            const {data} = await axios.get("http://localhost:5000/get-message");
+            setMessages(prevState => [data, ...prevState]);
+            await subscribe();
+        } catch (e) {
+            console.log(e);
+            setTimeout(() => {
+                subscribe();
+            }, 500);
+        }
+    }
 
     const sendMessage = async () => {
         await axios.post("http://localhost:5000/new-message", {
